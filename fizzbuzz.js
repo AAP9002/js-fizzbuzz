67ebbc9d016a5ec33fe666d03ab6bd3ce501f4e3
Alan Prophett <alan.prophett@softwire.com>
2024-06-24 17:02:48 +0100

    impliment fizz buzz 0 to 100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,22 @@
+// mod function
+function mod(i,k){
+    return i%k == 0
+}
+
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
-    // Put your code here...
+   for(var i = 0; i < 100; i++){
+    if(mod(i,3) && mod(i,5)){
+        console.log("FizzBuzz")
+    }else if(mod(i,3)){
+        console.log("Fizz")
+    }else if(mod(i,5)){
+        console.log("Buzz")
+    }
+    else{
+        console.log(i)
+    }
+   }
 
 }
 
