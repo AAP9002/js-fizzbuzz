7d30bc882c143f4c63b501be9fcd705821191300
Alan Prophett <alan.prophett@softwire.com>
2024-06-24 17:31:48 +0100

    fix include bang for multiples of 7

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,30 @@
 // mod function
 function mod(i,k){
-    return i%k == 0
+    return i%k === 0
 }
 
 // This is our main function
 function fizzbuzz() {
-   for(var i = 0; i < 100; i++){
-    if(mod(i,3) && mod(i,5)){
-        console.log("FizzBuzz")
-    }else if(mod(i,3)){
-        console.log("Fizz")
-    }else if(mod(i,5)){
-        console.log("Buzz")
+    for(var i = 0; i < 100; i++){
+        var words = []
+
+    if(mod(i,3)){
+        words.push("Fizz")
+    }
+    
+    if(mod(i,5)){
+        words.push("Buzz")
+    }
+    
+    if(mod(i,7)){
+        words.push("Bang")
+    }
+
+    if (words.length === 0){
+        console.log(i)
     }
     else{
-        console.log(i)
+        console.log(words.join(''))
     }
    }
 
